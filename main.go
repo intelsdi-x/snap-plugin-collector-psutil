c042e663e8f40718837480da28504e058fcaa1aa
geauxvirtual <justin.guidroz@gmail.com>
2015-10-09 13:46:07 -0700

    Fix import

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	// Import the pulse plugin library
 	"github.com/intelsdi-x/pulse/control/plugin"
 	// Import our collector plugin implementation
-	"github.com/intelsdi-x/pulse-plugin-collector-psutil"
+	"github.com/intelsdi-x/pulse-plugin-collector-psutil/psutil"
 )
 
 // plugin bootstrap
