0e7ab82a28c6ec3a1ff72ed80165c3c842b1d674
Marcin Krolik <marcin.krolik@intel.com>
2016-09-29 14:02:17 +0200

    Env vars for task, plugins and snap path

---
+++ scripts/large_k8s.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#
+#Copyright 2016 Intel Corporation
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+
+set -e
+set -u
+set -o pipefail
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__proj_dir="$(dirname "$__dir")"
+__proj_name="$(basename $__proj_dir)"
+
+. "${__dir}/common.sh"
+
+_info "DIR=${__dir}"
+_info "PROJ_DIR=${__proj_dir}"
+_info "PROJ_NAME${__proj_name}"
+
+#_info "updating repository submodule with pytest"
+cd ${__proj_dir} && git submodule update --init --recursive
+
+export PROJECT_NAME="${__proj_name}"
+
+#_info "execute large test"
+test_result=`python "${__proj_dir}/scripts/test/large.py"`
+exit $test_result
