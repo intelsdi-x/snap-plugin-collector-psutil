f77a755006534ddf889e834a8db0d4fd2ee0a990
Marcin Krolik <marcin.krolik@intel.com>
2016-12-14 12:03:17 +0100

    Example reorganization and large tests update

--- examples/run-file-psutil.sh
+++ examples/run-file-psutil.sh
@@ -6,14 +6,14 @@ set -o pipefail
 
 # get the directory the script exists in
 #__dir="$(cd ../../scripts/docker/large && pwd)"
-__dir=$(cd $(dirname ${BASH_SOURCE[0]})/../../scripts/docker/large && pwd)
-__proj_dir="$(cd $(dirname ${BASH_SOURCE[0]})/../../ && pwd)"
+__dir=$(cd $(dirname ${BASH_SOURCE[0]})/../scripts/docker/large && pwd)
+__proj_dir="$(cd $(dirname ${BASH_SOURCE[0]})/../ && pwd)"
 __proj_name="$(basename $__proj_dir)"
 
 export PLUGIN_SRC="${__proj_dir}"
 
 # verifies dependencies and starts influxdb
-. "${__proj_dir}/examples/tasks/.setup.sh"
+. "${__proj_dir}/examples/.setup.sh"
 
 # downloads plugins, starts snap, load plugins and start a task
 __id=$(docker run -e SNAP_VERSION=latest -d -v ${PLUGIN_SRC}:/${__proj_name} --net=host mkrolik/snap-pytest)
@@ -23,4 +23,4 @@ function finish {
 }
 trap finish EXIT INT TERM
 
-docker exec -it ${__id} bash -c "PLUGIN_PATH=/etc/snap/plugins /${__proj_name}/examples/tasks/mock-psutil.sh && printf \"\n\nhint: type 'snaptel task list'\ntype 'exit' when your done\n\n\" && bash"
+docker exec -it ${__id} bash -c "PLUGIN_PATH=/etc/snap/plugins /${__proj_name}/examples/file-psutil.sh && printf \"\n\nhint: type 'snaptel task list'\ntype 'exit' when your done\n\n\" && bash"
